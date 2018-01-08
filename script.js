df9cd62d749bee949c31af1201be743d0f138617
dudkahrudka <stvrtecka.dominika@gmail.com>
2018-01-08 13:25:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var deadline;
+var submitBtn = document.getElementById('submit')
+
+submitBtn.addEventListener("click", function() {
+  var years = document.getElementById('years').value
+  var months = document.getElementById('months').value
+  var days = document.getElementById('days').value
+  var hours = document.getElementById('hours').value
+  var minutes = document.getElementById('minutes').value
+  var seconds = document.getElementById('seconds').value
+  deadline = months + " " + days + " " + years + " " + hours + ":" + minutes + ":" + seconds + " GMT+0100";
+  initializeClock('clockdiv', deadline);
+});
+
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
